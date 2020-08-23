e4a4921240929d05265fdb103728fd02477f0c23
Sam Lavin <salavin12@gmail.com>
2020-08-23 02:46:26 +0100

    Added new commands

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import discord
+import random
 
 TOKEN = 'NzQ2ODQxNTE4NjcyOTY5Nzc5.X0GMXQ.HahdiAEzgxz1C9NrZHhAh4Bocxo'
 
@@ -16,26 +17,43 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if 'cool' in message.content:
+    tmpmessage = message.content.lower()
+
+    if 'cool' in tmpmessage:
         await message.channel.send('Ice Cold!')
 
-    if 'Go Cyclones' in message.content:
+    if 'go cyclones' in tmpmessage:
         await message.channel.send('Yeah! Cyclones!')
 
-    if 'We love the Cyclones' in message.content:
+    if 'we love the cyclones' in tmpmessage:
         await message.channel.send('Yeah! Love')
 
-    if 'sense' in message.content:
+    if 'sense' in tmpmessage:
         await message.channel.send('Dollars!')
 
-    if 'dig' in message.content:
+    if 'dig' in tmpmessage:
         await message.channel.send('With a shovel!')
 
-    if 'super' in message.content:
+    if 'super' in tmpmessage:
         await message.channel.send('Super duper dad!')
 
-    if 'step show' in message.content:
+    if 'step show' in tmpmessage:
         await message.channel.send('cancelled')
+
+    if 'agree' in tmpmessage:
+        await message.channel.send('stop.')
+
+    if 'pregame' in tmpmessage:
+        if random.randrange(0,6,1) == 5:
+            await message.channel.send('ON the field :wink:')
+        else:
+            await message.channel.send('Off the field!')
+
+    if 'rise sons' in tmpmessage:
+        await message.channel.send('Starts with drums!')
+
+    if ('box' in tmpmessage) and ('box' in tmpmessage) and ('?' in tmpmessage):
+        await message.channel.send('Box link: https://iastate.box.com/v/ISUCFVMB2020')
 
 client.run(TOKEN)
 
