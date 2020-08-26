afd0c6fddbe34f22b768e0e36eabc527b222f42a
Sam Lavin <salavin12@gmail.com>
2020-08-26 14:03:23 -0500

    Formatting changes

--- bot.py
+++ bot.py
@@ -6,13 +6,13 @@ from datetime import datetime
 TOKEN = 'NzQ2ODQxNTE4NjcyOTY5Nzc5.X0GMXQ.HahdiAEzgxz1C9NrZHhAh4Bocxo'
 weatherUrl = "https://api.openweathermap.org/data/2.5/weather?zip=50012,us&appid=7627fa673f7ae31176e1373748ff78ac"
 
-
 forecastUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=42.031257&lon=-93.652086&appid=7627fa673f7ae31176e1373748ff78ac"
-timeformat = "%A %I:%M%p"
+timeFormat = "%A %I:%M%p"
 
 client = discord.Client()
 
-client.agreeCounter = 0 #i bound it to the client var because of wack scope issues
+client.agreeCounter = 0  # i bound it to the client var because of wack scope issues
+
 
 @client.event
 async def on_ready():
@@ -20,6 +20,7 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
+
 
 @client.event
 async def on_message(message):
@@ -49,16 +50,16 @@ async def on_message(message):
     if 'step show' in tmpmessage:
         await message.channel.send('Cancelled.')
 
-    if (tmpmessage == 'agree'):
+    if tmpmessage == 'agree':
         client.agreeCounter += 1
-        if (client.agreeCounter == 5):
+        if client.agreeCounter == 5:
             client.agreeCounter = 0
             await message.channel.send('stop.')
     else:
         client.agreeCounter = 0
 
     if 'pregame' in tmpmessage:
-        if random.randrange(0,6) == 5:
+        if random.randrange(0, 6) == 5:
             await message.channel.send('ON the field :wink:')
         else:
             await message.channel.send('Off the field!')
@@ -76,7 +77,7 @@ async def on_message(message):
         await message.channel.send('GO STATE')
 
     if 'carichnerbot' in tmpmessage:
-        if ('love' in tmpmessage):
+        if 'love' in tmpmessage:
             await message.channel.send('I love you too, <@' + str(message.author.id) + '> :heart:')
         elif ('hello' in tmpmessage) or ('hi' in tmpmessage):
             await message.channel.send('Hello <@' + str(message.author.id) + '>')
@@ -88,7 +89,7 @@ async def on_message(message):
         await message.channel.send('Where are we going?')
 
     if "cyclone power" in tmpmessage:
-        tmpNum = random.randrange(1,8)
+        tmpnum = random.randrange(1, 8)
         switcher = {
             1: "Take a shower?",
             2: "Eiffel Tower?",
@@ -98,7 +99,7 @@ async def on_message(message):
             6: "Eisenhower?",
             7: "Protein powder?"
         }
-        await message.channel.send(switcher.get(tmpNum, "Oh no! I threw an error!"))
+        await message.channel.send(switcher.get(tmpnum, "Oh no! I threw an error!"))
 
     if "cyclone!" in tmpmessage:
         await message.channel.send("Power!")
@@ -110,8 +111,9 @@ async def on_message(message):
     if "carichner" in tmpmessage:
         chris = '<:chris:746792499812761606>'
         await message.add_reaction(chris)
-        
-    if ("is it a good day for band" in tmpmessage) or ("is it a great day for band" in tmpmessage) or ("is it going to rain" in tmpmessage):
+
+    if ("is it a good day for band" in tmpmessage) or ("is it a great day for band" in tmpmessage) or (
+            "is it going to rain" in tmpmessage):
         forecast = requests.get(forecastUrl).json()
         hourly = forecast['hourly']
         ms = ''
@@ -119,7 +121,7 @@ async def on_message(message):
             timestamp = datetime.fromtimestamp(hour['dt'])
             if timestamp.hour == 17:
                 temp = str(round((hour['temp'] - 273.15) * 9.0 / 5 + 32, 1))
-                ms += 'On ' + timestamp.strftime(timeformat) + ' it will be ' + temp + '°F with a '
+                ms += 'On ' + timestamp.strftime(timeFormat) + ' it will be ' + temp + '°F with a '
                 ms += hour['weather'][0]['description'] + '\n'
                 ms += 'Looks like a GREAT day for a band rehearsal!'
                 await message.channel.send(ms)
@@ -134,9 +136,8 @@ async def on_message(message):
 
     if (tmpmessage == '2') or (tmpmessage == 'two'):
         await message.channel.send("Buh!")
-        
+
     if '!roll' in tmpmessage:
         await message.channel.send(str(random.randint(1, 100)))
 
 client.run(TOKEN)
-
